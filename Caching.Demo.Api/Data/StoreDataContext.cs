bba3805ec3b9b948411e62a56f9087e4b1b93874
eduardosm7 <esm7@cin.ufpe.br>
2019-11-18 21:35:17 -0300

    Add environment variables and Redis on docker-compose

--- Caching.Demo.Api/Data/StoreDataContext.cs
+++ Caching.Demo.Api/Data/StoreDataContext.cs
@@ -1,3 +1,4 @@
+using System;
 using Caching.Demo.Api.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -7,9 +8,14 @@ namespace Caching.Demo.Api.Data
     {
         public DbSet<Product> Products { get; set; }
 
+        private string DB_HOST = Environment.GetEnvironmentVariable("DB_HOST");
+        private string DB_NAME = Environment.GetEnvironmentVariable("DB_NAME");
+        private string DB_USER = Environment.GetEnvironmentVariable("DB_USER");
+        private string DB_PASSWORD = Environment.GetEnvironmentVariable("DB_PASSWORD");
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseNpgsql("Host=caching-demo-psql;Database=caching-demo-psql;Username=admin;Password=admin");
+            optionsBuilder.UseNpgsql($"Host={DB_HOST};Database={DB_NAME};Username={DB_USER};Password={DB_PASSWORD}");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
